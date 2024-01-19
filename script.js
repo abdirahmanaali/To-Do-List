eb1c35755c439f5cdceb3b32ebb2852d07775330
Abdirahman Ali <abdirahmanabdiaziz@gmail.com>
2024-01-19 11:56:27 -0600

    worked on add task as well as clear all btns

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ const taskInput = document.getElementById("task-input");
 const addTask = document.getElementById("add-task");
 const taskList = document.getElementById("task-list");
 const emptyMessage = document.getElementById("empty-message");
-const clearAll = document.getElementById("clear-all");
+const clearAll = document.querySelector(".clear-all");
 
 // prevents the page from loading
 taskForm.addEventListener("submit", function (event) {
   event.preventDefault();
 });
-
+console.log(taskList);
 // add task btn eventlistener
-addTask.addEventListener("click", function () {
+addTask.addEventListener("click", function (event) {
+  event.preventDefault();
   // is task input empty?
   const inputText = taskInput.value.trim();
-  console.log(inputText);
   if (inputText !== "") {
+    emptyMessage.style.display = "none";
+
     // create a list to put task items
     const task = document.createElement("li");
     console.log(task);
@@ -24,11 +26,12 @@ addTask.addEventListener("click", function () {
     // add a delete btn into the each task
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("clear-all");
 
     // remove the task from list once delete btn is clicked
     deleteBtn.addEventListener("click", function () {
       taskList.removeChild(task);
-      checkTaskList;
+      checkTaskList();
     });
 
     // task items become child of ul to show dynamically
@@ -38,19 +41,19 @@ addTask.addEventListener("click", function () {
   }
   // clear the input field
   taskInput.value = "";
-  emptyMessage.style.display = "none";
 });
 
 clearAll.addEventListener("click", function () {
   taskList.innerHTML = "";
   taskInput.value = "";
-  emptyMessage.style.display = "none";
-  checkTaskList;
+  emptyMessage.textContent = "No task yet. Add a new task above";
+  checkTaskList();
 });
 
 function checkTaskList() {
   if (taskList.children.length === 0) {
     emptyMessage.style.display = "block";
+    emptyMessage.textContent = "No task yet. Add a new task above";
   } else {
     emptyMessage.style.display = "none";
   }
